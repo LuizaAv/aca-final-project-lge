2be36de42a7e4386f0bfb772abf9ac7d0d8aacae
yeghishehov <yeghishehov.1@gmail.com>
2020-03-19 20:58:41 +0400

    ref(*) improved code structure

--- src/components/pages/Spending/Spending.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Spending() {
-  return <div> Spending </div>;
-}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 
-import Spending from './pages/Spending/Spending';
-import Categories from './pages/Categories/Categories';
-import History from './pages/History/History';
+import Summary from './Summary/Summary';
+import Categories from './Categories/Categories';
+import History from './History/History';
 
 export default function Main() {
   return (
     <div>
-      <Link to="/">Spending</Link>
+      <Link to="/">Summary</Link>
       <Link to="/Categories">Categories</Link>
       <Link to="/History">History</Link>
 
       <Switch>
         <Route exact path="/">
-          <Spending />
+          <Summary />
         </Route>
 
         <Route path="/Categories">
