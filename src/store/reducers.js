d1b185f0ffde6a96f108d4ed334aa02d4c3133e1
yeghishehov <yeghishehov.1@gmail.com>
2020-03-20 14:26:12 +0400

    feat(reducers) added removе and edit functionality

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,23 @@
-import { ADD_CATEGORY, ADD_BUDGET } from './actions';
+import {
+  ADD_CATEGORY,
+  REMOVE_CATEGORY,
+  EDIT_CATEGORY,
+  ADD_BUDGET,
+  REMOVE_BUDGET,
+  EDIT_BUDGET,
+} from './actions';
 
 export function categoriesReducer(state, action) {
   switch (action.type) {
     case ADD_CATEGORY:
       return [...state, action.payload];
+    case REMOVE_CATEGORY:
+      return state.filter((category) => category.id !== action.payload.id);
+    case EDIT_CATEGORY:
+      return state.map((category) => (
+        category.id === action.payload.id
+          ? action.payload
+          : category));
     default:
       return state;
   }
@@ -13,6 +27,13 @@ export function budgetReducer(state, action) {
   switch (action.type) {
     case ADD_BUDGET:
       return [...state, action.payload];
+    case REMOVE_BUDGET:
+      return state.filter((budget) => budget.id !== action.payload.id);
+    case EDIT_BUDGET:
+      return state.map((budget) => (
+        budget.id === action.payload.id
+          ? action.payload
+          : budget));
     default:
       return state;
   }
