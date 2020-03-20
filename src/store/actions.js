852b443a139fb12ca4b21ea686664ca8771aa281
yeghishehov <yeghishehov.1@gmail.com>
2020-03-20 13:14:27 +0400

    feta(actions) added remove and edit

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,31 @@
 export const ADD_CATEGORY = 'ADD_CATEGORY';
-export const ADD_BUDGET = 'ADD_BUDGET';
+export const REMOVE_CATEGORY = 'REMOVE_CATEGORY';
+export const EDIT_CATEGORY = 'EDIT_CATEGORY';
 
+export const ADD_BUDGET = 'ADD_BUDGET';
+export const REMOVE_BUDGET = 'ADD_BUDGET';
+export const EDIT_BUDGET = 'ADD_BUDGET';
 
 export function addCategory(payload) {
   return { type: ADD_CATEGORY, payload };
 }
 
+export function removeCategory(payload) {
+  return { type: REMOVE_CATEGORY, payload };
+}
+
+export function editCategory(payload) {
+  return { type: EDIT_CATEGORY, payload };
+}
+
 export function addBudget(payload) {
   return { type: ADD_BUDGET, payload };
+}
+
+export function removeBudget(payload) {
+  return { type: REMOVE_BUDGET, payload };
+}
+
+export function editBudget(payload) {
+  return { type: EDIT_BUDGET, payload };
 }
