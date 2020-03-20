f629a8953813327d60322b39d472fc6835a2fe24
yeghishehov <yeghishehov.1@gmail.com>
2020-03-20 23:44:19 +0400

    feat(AddCategory) add Material UI Dialog

--- src/pages/Categories/AddCategory.js
+++ src/pages/Categories/AddCategory.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
 
 import { useStoreContext } from '../../store/storeContext';
 import { addCategory } from '../../store/actions';
 
 export default function AddCategory() {
   const { state, dispatch } = useStoreContext();
+
   const [type, setType] = React.useState('');
   const [name, setName] = React.useState('');
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(!open);
+  };
 
   return (
-    <button type="button">
-      add
-    </button>
+    <>
+      <button type="button">
+        add
+      </button>
+
+      <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+        <input type='text' />
+      </Dialog>
+    </>
   );
 }
