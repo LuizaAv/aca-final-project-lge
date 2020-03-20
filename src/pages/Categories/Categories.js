19b51a65e78c7be1eeac4465e354d97960e450f8
yeghishehov <yeghishehov.1@gmail.com>
2020-03-20 21:58:38 +0400

    feat(Categories) categories shown

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { useStoreContext } from '../../store/storeContext';
+
 export default function Categories() {
+  const { state } = useStoreContext();
+
   return (
-    <div> Categories </div>
+    <>
+      <ul>
+        {state.categories.map(category => (
+          <li>{category.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
