77b8c1945fb2ead25a7d687a5a825b93cdaefe86
Luiza Avetisyan <l.avetisyan7777@gmail.com>
2020-03-21 21:56:47 +0400

    added select category

--- src/Components/AddBudget/AddBudget.js
+++ src/Components/AddBudget/AddBudget.js
@@ -2,11 +2,8 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import InputBase from "@material-ui/core/InputBase";
 
 function AddBudget() {
   const [open, setOpen] = React.useState(false);
