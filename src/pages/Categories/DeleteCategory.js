6dade7dccab7e80bedcdab8be20b7ca05f1193f5
yeghishehov <yeghishehov.1@gmail.com>
2020-03-23 00:04:20 +0400

    feat(DeleteComponent) added delete functionality

--- src/pages/Categories/DeleteCategory.js
+++ src/pages/Categories/DeleteCategory.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 
 import { useStoreContext } from '../../store/storeContext';
 import { deleteCategory } from '../../store/actions';
 
-export default function RemoveCategory() {
-  const { state, dispatch } = useStoreContext();
+export default function DeleteCategory({ category }) {
+  const { dispatch } = useStoreContext();
+
+  const handleDeleteCategory = () => {
+    dispatch(deleteCategory(category));
+  };
 
   return (
-    <button type="button">
-      remove
-    </button>
+    <Button variant="outlined" onClick={handleDeleteCategory}>
+      Delete
+    </Button>
   );
 }
+
+DeleteCategory.propTypes = {
+  category: propTypes.shape({
+    id: propTypes.number.isRequired,
+    type: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+  }),
+};
+
+DeleteCategory.defaultProps = {
+  category: {
+    id: propTypes.number.isRequired,
+    type: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+  },
+};
