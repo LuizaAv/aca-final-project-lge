078cc08a8f290adcc1c9329e66f349ec52bd5dea
Luiza Avetisyan <l.avetisyan7777@gmail.com>
2020-03-24 00:53:10 +0400

    corrected Cards

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -11,40 +11,40 @@ const useStyles = makeStyles({
     minWidth: 275,
     margin: 10,
     width: 500,
-    height: 200,
+    height: 140,
     border: "1.5px solid black",
-    borderRadius: 10,
+    borderRadius: 10
   },
   bullet: {
     display: "inline-block",
     margin: "0 2px",
-    transform: "scale(0.8)",
+    transform: "scale(0.8)"
   },
   item: {
     fontSize: 14,
     float: "left",
-    marginLeft: 40,
+    marginLeft: 40
   },
   price: {
     fontSize: 14,
-    marginLeft: 290,
+    marginLeft: 290
   },
   date: {
     fontSize: 14,
     marginLeft: 290,
-    marginTop: -25,
+    marginTop: -25
   },
   pos: {
-    marginBottom: 12,
+    marginBottom: 12
   },
   category: {
     marginLeft: 40,
-    marginTop: 20,
+    marginTop: 20
   },
   line: {
     marginTop: 40,
-    backgroundColor: "yellow",
-  },
+    backgroundColor: "yellow"
+  }
 });
 
 function History() {
@@ -53,63 +53,43 @@ function History() {
 
   return (
     <div>
-      <Card className={classes.root} variant="outlined">
-        <CardContent>
-          {state.budget.map(item => item.id > 8 ? (
-            <Typography
+      {state.budget.map(item =>
+        item.id > 8 ? (
+          <Card className={classes.root} variant="outlined">
+            <CardContent>
+              <Typography
                 className={classes.item}
                 color="textSecondary"
                 gutterBottom
               >
-                Name:
-                {' '}
-                {item.name}
+                Name: {item.name}
               </Typography>
-          ) : null,
-          )}
-
-          {state.budget.map(item => item.id > 8 ? (
-            <Typography
+              <Typography
                 className={classes.price}
                 color="textSecondary"
                 gutterBottom
               >
-                Amount:
-                {' '}
-                {item.amount}
+                Amount: {item.amount}
               </Typography>
-          ) : null,
-          )}
-
-          <hr className={classes.line} />
-
-          {state.budget.map(item => item.id > 8 ? (
-            <Typography
+              <hr className={classes.line} />
+              <Typography
                 className={classes.category}
                 color="textSecondary"
                 gutterBottom
               >
-                Category:
-                {' '}
-                {item.category}
+                Category: {item.category}
               </Typography>
-          ) : null,
-          )}
-
-          {state.budget.map(item => item.id > 8 ? (
-            <Typography
+              <Typography
                 className={classes.date}
                 color="textSecondary"
                 gutterBottom
               >
-                Date:
-                {' '}
-                {item.date}
+                Date: {item.date}
               </Typography>
-          ) : null,
-          )}
-        </CardContent>
-      </Card>
+            </CardContent>
+          </Card>
+        ) : null
+      )}
     </div>
   );
 }
