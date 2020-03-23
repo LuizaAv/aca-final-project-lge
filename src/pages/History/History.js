c9d54e630ba7c035cf32b893bba599ddd9d99729
Luiza Avetisyan <l.avetisyan7777@gmail.com>
2020-03-24 01:57:01 +0400

    corrected code trough ESLINT

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Emptypage from './Emptyhistorypage'
-import { useStoreContext } from "../../store/storeContext";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Emptypage from './Emptyhistorypage';
+import { useStoreContext } from '../../store/storeContext';
 
 const useStyles = makeStyles({
   root: {
@@ -12,39 +12,39 @@ const useStyles = makeStyles({
     margin: 10,
     width: 500,
     height: 140,
-    border: "1.5px solid black",
-    borderRadius: 10
+    border: '1.5px solid black',
+    borderRadius: 10,
   },
   bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
   },
   item: {
     fontSize: 14,
-    float: "left",
-    marginLeft: 40
+    float: 'left',
+    marginLeft: 40,
   },
   price: {
     fontSize: 14,
-    marginLeft: 290
+    marginLeft: 290,
   },
   date: {
     fontSize: 14,
     marginLeft: 290,
-    marginTop: -25
+    marginTop: -25,
   },
   pos: {
-    marginBottom: 12
+    marginBottom: 12,
   },
   category: {
     marginLeft: 40,
-    marginTop: 20
+    marginTop: 20,
   },
   line: {
     marginTop: 40,
-    backgroundColor: "yellow"
-  }
+    backgroundColor: 'yellow',
+  },
 });
 
 function History() {
@@ -53,43 +53,50 @@ function History() {
 
   return (
     <div>
-      { state.budget.map(item =>
-          <Card className={classes.root} variant="outlined">
-            <CardContent>
-              <Typography
-                className={classes.item}
-                color="textSecondary"
-                gutterBottom
-              >
-                Name: {item.name}
-              </Typography>
-              <Typography
-                className={classes.price}
-                color="textSecondary"
-                gutterBottom
-              >
-                Amount: {item.amount}
-              </Typography>
-              <hr className={classes.line} />
-              <Typography
-                className={classes.category}
-                color="textSecondary"
-                gutterBottom
-              >
-                Category: {item.category}
-              </Typography>
-              <Typography
-                className={classes.date}
-                color="textSecondary"
-                gutterBottom
-              >
-                Date: {item.date}
-              </Typography>
-            </CardContent>
-          </Card>
-      )
-      }
-    
+      { state.budget.map((item) => (
+        <Card className={classes.root} variant="outlined">
+          <CardContent>
+            <Typography
+              className={classes.item}
+              color="textSecondary"
+              gutterBottom
+            >
+              Name:
+              {' '}
+              {item.name}
+            </Typography>
+            <Typography
+              className={classes.price}
+              color="textSecondary"
+              gutterBottom
+            >
+              Amount:
+              {' '}
+              {item.amount}
+            </Typography>
+            <hr className={classes.line} />
+            <Typography
+              className={classes.category}
+              color="textSecondary"
+              gutterBottom
+            >
+              Category:
+              {' '}
+              {item.category}
+            </Typography>
+            <Typography
+              className={classes.date}
+              color="textSecondary"
+              gutterBottom
+            >
+              Date:
+              {' '}
+              {item.date}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+
     </div>
   );
 }
