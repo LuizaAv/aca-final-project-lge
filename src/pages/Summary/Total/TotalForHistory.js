6583e3898d4046da9e5aafee779e8634baf88848
gormeshinyan <gormeshinyan@mail.ru>
2020-04-01 23:39:09 +0400

    Create OnlyBalance Component

---
+++ src/pages/Summary/Total/TotalForHistory.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
+
+import { useStoreContext } from '../../../store/storeContext';
+
+import useStyles from './TotalForHistroy.style'
+
+export default function Total() {
+  const classes = useStyles();
+  const { state } = useStoreContext();
+
+  const income = state.budget.reduce((acc, budget) => (
+    budget.type === 'income'
+      ? acc + +budget.amount
+      : acc
+  ), 0);
+
+  const expense = state.budget.reduce((acc, budget) => (
+    budget.type === 'expense'
+      ? acc + +budget.amount
+      : acc
+  ), 0);
+
+  const balance = income - expense;
+
+  const addComma = (count) => count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  const count = [
+    { name: 'B A L A N C E', amount: addComma(balance), className: 'balance' },
+    
+  ];
+
+  return (
+    <div className={classes.root}>
+      {count.map((item) => (
+        <Typography key={item.name} className={clsx(classes.count, classes[item.className])}>
+          {item.amount}
+          <Typography className={classes.name}>
+            {item.name}
+          </Typography>
+        </Typography>
+      ))}
+    </div>
+  );
+}
---
+++ src/pages/Summary/Total/TotalForHistroy.style.js
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  root: {
+    display: 'flex',
+    
+  },
+  count: {
+    
+    paddingLeft: 40,
+    marginLeft: 50,
+    fontFamily: 'Segoe UI',
+    fontWeight: 900,
+    fontSize: 30,
+    letterSpacing: 2,
+    transform: 'scaleY(0.9)',
+  },
+  balance: {
+    color: 'red',
+    marginLeft:-310,
+  },
+  name: {
+    color: '#545454',
+    fontFamily: 'Segoe UI',
+    fontWeight: 900,
+  },
+  
+});
