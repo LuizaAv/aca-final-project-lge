4e3b606900a9e87fbdc40883abe6cb2eb8597dc8
yeghishehov <yeghishehov.1@gmail.com>
2020-04-01 16:45:53 +0400

    style(Categories) css moved to a separate file

--- src/pages/Categories/Categories.style.js
+++ src/pages/Categories/Categories.style.js
@@ -1,20 +1,58 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  head: {
-    backgroundColor: '#466d6d',
-    color: 'white',
-    fontSize: 20,
-  },
-  flexContainer: {
+  root: {
     display: 'flex',
-    justifyContent: 'center',
-    padding:12,
-    
+    flexDirection: 'column',
+  },
+  header: {
+    display: 'flex',
+    // alignItems: 'center',
+    justifyContent: 'flex-end',
+    width: '96%',
+  },
+  tools: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: 30,
+    marginLeft: 85,
+    width: 300,
+  },
+  tableContainer: {
+    borderRadius: 30,
+    width: '92%',
+    alignSelf: 'center',
+  },
+  title: {
+    color: '#363D99',
+    fontFamily: 'Segoe UI',
+    fontSize: 30,
+    fontWeight: 900,
+    marginLeft: 50,
+    marginTop: 30,
   },
   table: {
-    width: 600,
-    marginTop:-5,
+    width: '94%',
+    margin: 'auto',
+  },
+  icon: {
+    marginRight: 10,
+    marginBottom: -4,
+  },
+  head: {
+    fontFamily: 'Segoe UI',
+    fontWeight: 900,
+    fontSize: 17,
+    width: '20%',
+  },
+  content: {
+    fontFamily: 'Segoe UI',
+    fontSize: 17,
+  },
+  name: {
+    color: '#363D99',
+    fontSize: 17,
   },
 
   tableRow: {
@@ -25,10 +63,5 @@ export default makeStyles((theme) => ({
       transform: 'scale(1.1)',
       backgroundColor: '#e0ebeb',
     },
-  },
-  moneyIcon: {
-    alignItems: 'baseline',
-    color: '#white',
-   
   },
 }));
