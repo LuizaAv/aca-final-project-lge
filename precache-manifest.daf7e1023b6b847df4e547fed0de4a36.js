7abae0278ff2ca3e3a1f0512a328eb3c8c5921a9
Luiza Avetisyan <l.avetisyan7777@gmail.com>
2020-04-02 22:47:19 +0400

    Updates

---
+++ precache-manifest.daf7e1023b6b847df4e547fed0de4a36.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec891f760212abda54612144b5613f3",
+    "url": "/aca-final-project-lge/index.html"
+  },
+  {
+    "revision": "c6279343eecb0062fb3f",
+    "url": "/aca-final-project-lge/static/css/main.48d3cb6e.chunk.css"
+  },
+  {
+    "revision": "3c77272b135910a75771",
+    "url": "/aca-final-project-lge/static/js/2.fdb4999e.chunk.js"
+  },
+  {
+    "revision": "fb6d71085796588b665e9bcc04d93d26",
+    "url": "/aca-final-project-lge/static/js/2.fdb4999e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6279343eecb0062fb3f",
+    "url": "/aca-final-project-lge/static/js/main.3cf2b77e.chunk.js"
+  },
+  {
+    "revision": "b136d124ffad18adfceb",
+    "url": "/aca-final-project-lge/static/js/runtime-main.4e89c3f6.js"
+  },
+  {
+    "revision": "8ae78787c52e44ab7d534ef5715793e8",
+    "url": "/aca-final-project-lge/static/media/Arrow-down.8ae78787.svg"
+  },
+  {
+    "revision": "5ef66327633feef9e1a814c243acf22e",
+    "url": "/aca-final-project-lge/static/media/Arrow-up.5ef66327.svg"
+  },
+  {
+    "revision": "823d1c324b9315f4e68414af34f46951",
+    "url": "/aca-final-project-lge/static/media/Categories.823d1c32.svg"
+  },
+  {
+    "revision": "ec2435a4add409b6461c53d71f984af3",
+    "url": "/aca-final-project-lge/static/media/History.ec2435a4.svg"
+  },
+  {
+    "revision": "e453c90c1cd58aa078891d74b3388fd5",
+    "url": "/aca-final-project-lge/static/media/Rectangle.e453c90c.svg"
+  },
+  {
+    "revision": "6581cfaeee8057734a3f16d37719bfb2",
+    "url": "/aca-final-project-lge/static/media/SegoeUI.6581cfae.ttf"
+  },
+  {
+    "revision": "ea20dee138d57e5583e2c95fc678b6be",
+    "url": "/aca-final-project-lge/static/media/Summary.ea20dee1.svg"
+  }
+]);
