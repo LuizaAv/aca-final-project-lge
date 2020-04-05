a8e895ed535e85aa1ee11c113592c22b917cd8f3
Luiza Avetisyan <l.avetisyan7777@gmail.com>
2020-04-05 15:55:01 +0400

    corrected imports

--- src/components/DateFilter/DateFilter.js
+++ src/components/DateFilter/DateFilter.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+
 import useStyles from './DateFilter.style';
 
 
