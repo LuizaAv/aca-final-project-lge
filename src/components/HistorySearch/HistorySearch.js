29db08e740fa61c8498a6fdd341823e820ce3404
gormeshinyan <gormeshinyan@mail.ru>
2020-04-08 11:58:27 +0400

    Change Input Add Style

--- src/components/HistorySearch/HistorySearch.js
+++ src/components/HistorySearch/HistorySearch.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '22ch',
+      color:'#ff5d5d',
+    },
+  },
+}));
 
 export default function HistorySearch({e,setE}){
+    const classes = useStyles();
     const handleChange=(e)=>{
         setE(e.target.value) 
       }
      
     return (
-        <div >
-        <input  value={e} onChange={handleChange}/>
+        <div className={classes.root} noValidate autoComplete="off">
+         <TextField
+        id="outlined-secondary"
+        label="Search Your Card..."
+        variant="outlined"
+        color="secondary"
+        value={e} 
+        onChange={handleChange}
+      />
+       
         </div>
     )
 }
