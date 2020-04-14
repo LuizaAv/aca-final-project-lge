6ab8552fefc0aae1227be572133bcadf3c0ea033
yeghishehov <yeghishehov.1@gmail.com>
2020-04-14 16:25:38 +0400

    feta(API) add sjon-server actions

---
+++ src/API/dbActions.js
@@ -0,0 +1,71 @@
+const URL = 'http://localhost:3010';
+
+export function dbAddBudget(payload) {
+  fetch(`${URL}/budget`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbEditBudget(payload) {
+  fetch(`${URL}/budget/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbDeleteBudget(payload) {
+  fetch(`${URL}/budget/${payload.id}`, {
+    method: 'DELETE',
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbAddCategory(payload) {
+  fetch(`${URL}/categories`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbEditCategory(payload) {
+  fetch(`${URL}/categories/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbDeleteCategory(payload) {
+  fetch(`${URL}/categories/${payload.id}`, {
+    method: 'DELETE',
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
