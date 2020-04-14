cd0dcdca0d34d461644738f286b1460cd9e2d8b8
yeghishehov <yeghishehov.1@gmail.com>
2020-04-15 01:50:01 +0400

    feat(dbAction) add dbGet funcvtions

--- src/API/dbActions.js
+++ src/API/dbActions.js
@@ -1,38 +1,11 @@
 const URL = 'http://localhost:3010';
 
-export function dbAddBudget(payload) {
-  fetch(`${URL}/budget`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .catch((error) => console.error('Error:', error))
-    .then((response) => console.log('Success:', JSON.stringify(response)));
-}
-
-export function dbEditBudget(payload) {
-  fetch(`${URL}/budget/${payload.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .catch((error) => console.error('Error:', error))
-    .then((response) => console.log('Success:', JSON.stringify(response)));
-}
-
-export function dbDeleteBudget(payload) {
-  fetch(`${URL}/budget/${payload.id}`, {
-    method: 'DELETE',
-  })
-    .then((response) => response.json())
-    .catch((error) => console.error('Error:', error))
-    .then((response) => console.log('Success:', JSON.stringify(response)));
+export function dbGetCategory() {
+  return (
+    fetch(`${URL}/categories`)
+      .then((response) => response.json())
+      .catch((error) => console.error('Error:', error))
+  );
 }
 
 export function dbAddCategory(payload) {
@@ -63,6 +36,49 @@ export function dbEditCategory(payload) {
 
 export function dbDeleteCategory(payload) {
   fetch(`${URL}/categories/${payload.id}`, {
+    method: 'DELETE',
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbGetBudget() {
+  return (
+    fetch(`${URL}/budget`)
+      .then((response) => response.json())
+      .catch((error) => console.error('Error:', error))
+  );
+}
+
+export function dbAddBudget(payload) {
+  fetch(`${URL}/budget`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbEditBudget(payload) {
+  fetch(`${URL}/budget/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error('Error:', error))
+    .then((response) => console.log('Success:', JSON.stringify(response)));
+}
+
+export function dbDeleteBudget(payload) {
+  fetch(`${URL}/budget/${payload.id}`, {
     method: 'DELETE',
   })
     .then((response) => response.json())
