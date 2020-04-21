f4158cd009aaa9dffde3fa5b2135f3d9de4b3e21
yeghishehov <yeghishehov.1@gmail.com>
2020-04-21 15:23:24 +0400

    feat(Chart) add select chart type

---
+++ src/pages/Charts/ChartType/ChartType.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import useStyles from './ChartType.style';
+
+export default function ChartType({ type, setType }) {
+  const classes = useStyles();
+
+  const handleChange = (e) => {
+    setType(e.target.value);
+  };
+
+  return (
+    <FormControl variant="outlined">
+      <InputLabel className={classes.label}>
+        Chart type
+      </InputLabel>
+      <Select
+        className={classes.select}
+        classes={{ root: classes.selectRoot }}
+        variant="outlined"
+        defaultValue="date"
+        onChange={handleChange}
+        label="Chart type"
+      >
+        <MenuItem
+          className={classes.item}
+          value="date"
+        >
+          By date
+        </MenuItem>
+        <MenuItem
+          className={classes.item}
+          value="amount"
+        >
+          By amount
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
+ChartType.propTypes = {
+  type: propTypes.bool.isRequired,
+  setType: propTypes.func.isRequired,
+};
