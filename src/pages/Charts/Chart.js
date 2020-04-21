9d176ab1fc2be0423d99182274a3d1ce795291ea
yeghishehov <yeghishehov.1@gmail.com>
2020-04-21 15:24:10 +0400

    feat(Chart) add select chart type

--- src/pages/Charts/Chart.js
+++ src/pages/Charts/Chart.js
@@ -1,34 +1,47 @@
 import React, { useState } from 'react';
-import { Line } from 'react-chartjs-2';
+import { Line, Doughnut } from 'react-chartjs-2';
 import Paper from '@material-ui/core/Paper';
 
 import { useStoreContext } from '../../store/storeContext';
 import Show from '../../components/Show/Show';
+import CartType from './ChartType/ChartType';
 import useStyles from './Charts.style';
 
 export default function MainChart() {
   const classes = useStyles();
   const { state } = useStoreContext();
   const [isCurrent, setIsCurrent] = useState(true);
+  const [type, setType] = useState('date');
 
   const currentBudget = state.budget.filter((item) => item.date.getTime() <= new Date().getTime());
   const futureBudget = state.budget.filter((item) => item.date.getTime() > new Date().getTime());
 
   const showItems = isCurrent ? currentBudget : futureBudget;
 
+  const uniqueCategories = showItems.reduce((acc, item) => (
+    acc.some((accItem) => (
+      accItem.category === item.category
+      && accItem.type === item.type
+    ))
+      ? acc
+      : [...acc, item]
+  ), []);
+
   const sortedBudgetByDate = showItems.sort((a, b) => a.date.getTime() - b.date.getTime());
-  const labels = sortedBudgetByDate.map((el) => el.date.toLocaleDateString());
+  const labelsLine = sortedBudgetByDate.map((el) => el.date.toLocaleDateString());
   const expenses = sortedBudgetByDate.map((el) => (el.type === 'expense' ? el.amount : 0));
   const incomes = sortedBudgetByDate.map((el) => (el.type === 'income' ? el.amount : 0));
+  const labelsDoughnut = uniqueCategories.map((item) => item.name);
+  const amounts = uniqueCategories.map((item) => item.amount);
 
   if (!isCurrent) {
-    labels.unshift(new Date().toLocaleDateString());
+    labelsLine.unshift(new Date().toLocaleDateString());
     expenses.unshift(0);
     incomes.unshift(0);
   }
 
-  const data = {
-    labels,
+  const dataLine = {
+    labels: labelsLine,
     datasets: [
       {
         label: 'income',
@@ -47,6 +60,25 @@ export default function MainChart() {
     ],
   };
 
+  const dataDoughnut = {
+    labels: labelsDoughnut,
+    datasets: [
+      {
+        data: amounts,
+        backgroundColor: [
+          '#ff8a80', '#ff80ab', '#ea80fc', '#b388ff',
+          '#82b1ff', '#80d8ff', '#b9f6ca', '#ccff90',
+          '#f4ff81', '#ffd740', '#ff6e40', '#8d6e63',
+          '#90a4ae',
+        ],
+      },
+    ],
+    display: true,
+    position: 'top',
+    fullWidth: true,
+    reverse: false,
+  };
+
   return (
     <div className={classes.container}>
       <header className={classes.header}>
@@ -55,10 +87,13 @@ export default function MainChart() {
 
       <div className={classes.tools}>
         <Show isCurrent={isCurrent} setIsCurrent={setIsCurrent} />
+        <CartType type={type} setType={setType} />
       </div>
 
       <Paper className={classes.paper} elevation={5}>
-        <Line data={data} />
+        {type === 'date'
+          ? <Line data={dataLine} />
+          : <Doughnut data={dataDoughnut} />}
       </Paper>
     </div>
   );
