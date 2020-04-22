7cc6b2d963f626d2e5c336c003e7cee29fe2506f
yeghishehov <yeghishehov.1@gmail.com>
2020-04-22 22:16:30 +0400

    feat(HistoryDelete) changed delete message

--- src/pages/Categories/DeleteCategory/DeleteCategory.js
+++ src/pages/Categories/DeleteCategory/DeleteCategory.js
@@ -67,7 +67,7 @@ export default function DeleteCategory({
         classes={{ paper: classes.dialog }}
       >
         <DialogTitle className={classes.dialogTitle} disableTypography>
-          Are you sure want to permanently remove this item?
+          Are you sure you want to delete this item?
         </DialogTitle>
         <DialogActions className={classes.dialogAction}>
           <Button
