a59249c5d1019c7a07f14bfbff642af5d83f017c
yeghishehov <yeghishehov.1@gmail.com>
2020-04-28 02:21:11 +0400

    feat(Help.style) change heading scale size

--- src/pages/Help/Help.style.js
+++ src/pages/Help/Help.style.js
@@ -1,39 +1,36 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-    root: {
-        width: '75%',
-        margin:'auto',
-        marginTop:'100px',
-        [theme.breakpoints.down('sm')]: {
-            width: '100%',
-          },
-      },
-      heading: {
-        fontSize: theme.typography.pxToRem(15),
-        flexBasis: '33.33%',
-        flexShrink: 0,
-        fontWeight:700,
-        color:'#3949ab',
-        fontSize:'19px',
-        '&:hover': {
-            transform: 'scale(1.3)',
-            // backgroundColor: '#ffebee',
-            width:'100%',
-            marginLeft:30,
-          },
-       
-      },
-    h1:{
-        color:'#3949ab', 
-        textAlign:'center',
-         
+  root: {
+    width: '75%',
+    margin: 'auto',
+    marginTop: '100px',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
     },
-    paragraph:{
-     padding:10,
-     textAlign:'justify',
-     fontWeight:700,
-    }
-   
-    
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(19),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+    fontWeight: 700,
+    color: '#3949ab',
+    '&:hover': {
+      transform: 'scale(1.1)',
+      width: '100%',
+      marginLeft: 10,
+    },
+  },
+  h1: {
+    color: '#3949ab',
+    textAlign: 'center',
+
+  },
+  paragraph: {
+    padding: 10,
+    textAlign: 'justify',
+    fontWeight: 700,
+  },
+
+
 }));
