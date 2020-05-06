4fbdff08ceca9b6a1ef753dcbd628b47b50b5dd5
yeghishehov <yeghishehov.1@gmail.com>
2020-05-06 23:55:28 +0400

    feat(App) added SnackbarProvider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
-
+import SnackbarProvider from './components/Snackbars/SnackbarProvider';
 import theme from './theme/theme';
-
 import Main from './pages/Main';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <HashRouter>
-        <Main />
-      </HashRouter>
+      <SnackbarProvider>
+        <HashRouter>
+          <Main />
+        </HashRouter>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
