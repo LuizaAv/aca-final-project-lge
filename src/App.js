2d9a2c0ace0654297daab391ad5273fbaaa60d01
yeghishehov <yeghishehov.1@gmail.com>
2020-05-07 21:37:29 +0400

    feat(App) added LanguageProvider

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import SnackbarProvider from './components/Snackbars/SnackbarProvider';
 import StoreProvider from './store/StoreProvider';
+import LanguageProvider from './languages/LanguageProvider';
 import theme from './theme/theme';
 import Main from './pages/Main';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <StoreProvider>
-        <SnackbarProvider>
-          <HashRouter>
-            <Main />
-          </HashRouter>
-        </SnackbarProvider>
-      </StoreProvider>
+      <LanguageProvider>
+        <StoreProvider>
+          <SnackbarProvider>
+            <HashRouter>
+              <Main />
+            </HashRouter>
+          </SnackbarProvider>
+        </StoreProvider>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
